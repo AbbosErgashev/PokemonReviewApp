f46ffb14653c5258d6b7d0e88964011ad9c1a787
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-06-22 15:38:24 +0500

    added: review, reviewer POST in controller

--- PokemonReviewApp/Controllers/ReviewerController.cs
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -25,7 +25,7 @@ namespace PokemonReviewApp.Controllers
         {
             var reviewer = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
 
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             return Ok(reviewer);
@@ -56,10 +56,42 @@ namespace PokemonReviewApp.Controllers
             var reviewer = _mapper.Map<List<ReviewDto>>(
                 _reviewerRepository.GetReviewByReviewer(reviewerId));
 
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             return Ok(reviewer);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateReviewer([FromBody] ReviewerDto reviewerCreate)
+        {
+            if (reviewerCreate is null)
+                return BadRequest(ModelState);
+
+            var reviewer = _reviewerRepository.GetReviewers()
+                .Where(r => r.LastName.TrimStart().ToUpper() == reviewerCreate.LastName.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (reviewer is not null)
+            {
+                ModelState.AddModelError("", "Reviewer already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewerMap = _mapper.Map<Reviewer>(reviewerCreate);
+
+            if(!_reviewerRepository.CreateReviewer(reviewerMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while savin");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully created");
         }
     }
 }
